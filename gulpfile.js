899483b57cdac7e7f62535700a350e6b7ba53b9b
Andi Dittrich <andi@andidittrich.de>
2020-11-14 11:38:03 +0100

    bumped deps; replaced gulp-concat-util

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const _gulp = require('gulp');
 const _sass = require('gulp-sass');
 const _prettyError = require('gulp-prettyerror');
 const _cleanCSS = require('gulp-clean-css');
-const _concat = require('gulp-concat-util');
+const _concat = require('gulp-concat');
 
 _gulp.task('sass', function (){
     return _gulp.src('./assets/layout.scss')
